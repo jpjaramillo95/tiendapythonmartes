9be850727595ebd829e5afe9eea02f0084de60b1
Juan Esteban Hoyos <fgaviria.edu@gmail.com>
2025-03-04 18:49:41 -0500

    feat se crea main con opciones

---
+++ mian.py
@@ -0,0 +1,11 @@
+nombreVendedor=None
+productos=[]
+producto={}
+
+opcion=100
+
+print("Mercado")
+print("1.Crear lista mercado")
+print("5. Salir")
+while opcion != 5:
+    opcion=int(input("Digita una opción: "))
